8d7ed8142dd3d367f6ed1a53b727dfc1143ebc64
SofianD <doual.sofian@hotmail.com>
2023-04-22 19:33:27 +0200

    comment

--- src/buildCliRunner.ts
+++ src/buildCliRunner.ts
@@ -1,52 +1,55 @@
-import { readFileSync } from "fs";
-import { parseModule, parseScript } from "meriyah";
-import { Program } from "meriyah/dist/src/estree";
+// import { readFileSync } from "fs";
+// import { parseModule, parseScript } from "meriyah";
+// import { Program } from "meriyah/dist/src/estree";
 
-interface RunnerOptions {
-  /**
-   * Default: `true`
-   */
-  module?: boolean;
-}
+// const kImportStatementsType = ["VariableDeclaration", "ImportDeclaration"];
 
-const defaultBuilderOptions = {
-  module: true
-};
+// interface RunnerOptions {
+//   /**
+//    * Default: `true`
+//    */
+//   module?: boolean;
+// }
 
-export function buildRunner(currentFileName: string, customOptions: RunnerOptions = {}) {
-  const options = {
-    ...defaultBuilderOptions,
-    ...customOptions
-  };
+// const defaultBuilderOptions = {
+//   module: true
+// };
 
-  const parse = options.module ? parseModule : parseScript;
-  const currentFile = readFileSync(currentFileName, {
-    encoding: "utf-8",
-    flag: "r"
-  });
+// export function buildRunner(currentFileName: string, customOptions: RunnerOptions = {}) {
+//   const options = {
+//     ...defaultBuilderOptions,
+//     ...customOptions
+//   };
 
-  this.ast = parse(currentFile);
-}
+//   const parse = options.module ? parseModule : parseScript;
+//   const importStatementsType = options.module ? "ImportDeclaration" : "VariableDeclaration";
+//   const currentFile = readFileSync(currentFileName, {
+//     encoding: "utf-8",
+//     flag: "r"
+//   });
 
-export function* fetchVariable(program: Program, variableName: string) {
-  const body = program.body;
+//   this.ast = parse(currentFile);
+// }
 
-  for (const statement of body) {
-    if (statement.type !== "VariableDeclaration") {
-      continue;
-    }
+// export function* fetchVariable(program: Program, variableName: string) {
+//   const body = program.body;
 
-    const declaration = statement.declarations[0];
+//   for (const statement of body) {
+//     if (statement.type !== importStatementsType) {
+//       continue;
+//     }
 
-    if (declaration.type !== "VariableDeclarator" || (declaration.id as any).name !== variableName) {
-      continue;
-    }
+//     const declaration = statement.declarations[0];
 
-    yield {
-      name: (declaration.id as any).name,
-      path: (declaration.init as any).arguments[0].value
-    }
-  }
+//     if (declaration.type !== "VariableDeclarator" || (declaration.id as any).name !== variableName) {
+//       continue;
+//     }
 
-  throw new Error(`Could not find ${variableName} module declaration.`)
-}
+//     yield {
+//       name: (declaration.id as any).name,
+//       path: (declaration.init as any).arguments[0].value
+//     }
+//   }
+
+//   throw new Error(`Could not find ${variableName} module declaration.`)
+// }
